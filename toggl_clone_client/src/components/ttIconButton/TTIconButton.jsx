9424c6b78f59e3133a2f612709a6f2c768bd6c2b
Tao Hu <taohu2222@gmail.com>
2023-06-12 18:02:27 +0800

    Added TTIconButto represneting the iconbuttons without any background

---
+++ toggl_clone_client/src/components/ttIconButton/TTIconButton.jsx
@@ -0,0 +1,48 @@
+import styled from "@emotion/styled";
+import { Button } from "@mui/material";
+import { grey } from "@mui/material/colors";
+import React from "react";
+
+const StyledButton = styled(Button)(({ theme }) => ({
+  minWidth: 0,
+  padding: 2,
+  "&>svg": {
+    fontSize: theme.typography.h6.fontSize,
+    color: "var(--color)",
+  },
+  "&:hover > svg": {
+    color: "var(--color-hover)",
+  },
+  "&:focus > svg": {
+    color: "var(--color-focus)",
+  },
+}));
+
+const TTIconButton = ({
+  onClick,
+  children,
+  color,
+  colorHover,
+  colorFocus,
+  ...other
+}) => {
+  const style = {
+    "--color": color ?? grey[800],
+    "--color-hover": colorHover ?? grey[900],
+    "--color-focus": colorFocus ?? "",
+  };
+
+  return (
+    <StyledButton
+      disableElevation
+      disableRipple
+      disableTouchRipple
+      disableFocusRipple
+      style={style}
+    >
+      {children}
+    </StyledButton>
+  );
+};
+
+export default TTIconButton;
