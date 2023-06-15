53856373f3b93b8be96815fc60a6c1816bc1e26f
Tao Hu <taohu2222@gmail.com>
2023-06-15 21:13:46 +0800

    Added custom styled version of mui datecalender

---
+++ toggl_clone_client/src/components/TTDateCalender/TTDateCalender.jsx
@@ -0,0 +1,75 @@
+import styled from "@emotion/styled";
+import { grey } from "@mui/material/colors";
+import { DateCalendar, LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import React from "react";
+
+const StyledDatCalender = styled(DateCalendar)(({ theme }) => ({
+  "& .MuiPickersCalendarHeader-root": {
+    "& .MuiPickersCalendarHeader-label": {
+      fontSize: theme.typography.h5.fontSize,
+      fontWeight: 300,
+    },
+    "& .MuiPickersArrowSwitcher-root": {
+      "& .MuiButtonBase-root": {
+        borderRadius: "10px",
+        borderWidth: 0.2,
+        borderColor: grey[600],
+        marginLeft: theme.spacing(0.1),
+        borderStyle: "solid",
+        fontSize: "20px",
+      },
+    },
+  },
+  "& .MuiPickersSlideTransition-root": {
+    minHeight: 280,
+  },
+  "& .MuiDayCalendar-header": {
+    "& .MuiTypography-root ": {
+      alignItems: "end",
+    },
+  },
+  "& .MuiDayCalendar-monthContainer": {
+    "& .MuiDayCalendar-weekContainer": {
+      // borderTop: "1px solid green",
+      "&:after": {
+        content: '""',
+        position: "absolute",
+        width: "85%",
+        height: 1,
+        backgroundColor: grey[400],
+      },
+
+      "& .MuiButtonBase-root": {
+        height: 34,
+        borderRadius: "8px",
+        marginTop: 4,
+        marginBottom: 1,
+      },
+      "& .MuiPickersDay-today": {
+        borderColor: grey[500],
+        backgroundColor: grey[300],
+      },
+      "& .Mui-selected": {
+        backgroundColor: theme.palette.secondary.main,
+      },
+    },
+  },
+}));
+
+const TTDateCalender = (props) => {
+  return (
+    <LocalizationProvider adapterLocale={AdapterDateFns}>
+      <StyledDatCalender
+        slotProps={{
+          day: { disableRipple: true },
+          nextIconButton: { disableRipple: true },
+          previousIconButton: { disableRipple: true },
+        }}
+        {...props}
+      ></StyledDatCalender>
+    </LocalizationProvider>
+  );
+};
+
+export default TTDateCalender;
