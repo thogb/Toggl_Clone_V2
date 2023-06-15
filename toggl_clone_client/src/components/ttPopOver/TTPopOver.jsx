2a3b5040630ca8a374bf872d6ebf7b1fdd4220ca
Tao Hu <taohu2222@gmail.com>
2023-06-15 21:13:14 +0800

    Added custom popover

---
+++ toggl_clone_client/src/components/ttPopOver/TTPopOver.jsx
@@ -0,0 +1,42 @@
+import { Popover } from "@mui/material";
+import React from "react";
+
+const SIZES = {
+  md: 238,
+  lg: 338,
+};
+
+const TTPopOver = ({
+  anchorEl,
+  anchorOrigin,
+  onClose,
+  children,
+  size = "lg",
+  ...others
+}) => {
+  return (
+    <Popover
+      open={Boolean(anchorEl)}
+      anchorEl={anchorEl}
+      onClose={onClose}
+      anchorOrigin={anchorOrigin ?? { vertical: "bottom", horizontal: "left" }}
+      disableScrollLock
+      sx={{
+        "& .MuiBackdrop-root": {
+          backgroundColor: "transparent",
+        },
+
+        "& .MuiPaper-root": {
+          display: "flex",
+          px: 1 / 2,
+          ...(size !== "none" ? { width: `${SIZES[size]}px` } : {}),
+        },
+      }}
+      {...others}
+    >
+      {children}
+    </Popover>
+  );
+};
+
+export default TTPopOver;
