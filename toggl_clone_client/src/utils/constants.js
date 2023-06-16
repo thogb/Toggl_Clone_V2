ae7d9b88217d623ae6813f4a90cbc24dd443bf27
Tao Hu <taohu2222@gmail.com>
2023-06-16 21:44:55 +0800

    Some more constants for use on time and date format

--- toggl_clone_client/src/utils/constants.js
+++ toggl_clone_client/src/utils/constants.js
@@ -1,2 +1,6 @@
 export const DRAWER_WIDTH = 226;
 export const APPBAR_HEIGHT = 85;
+
+export const REFERENCE_DATE = new Date(1970, 0, 1, 0, 0, 0);
+export const DATE_FORMAT = "dd/MM/yyyy";
+export const TIME_FORMAT = "h:mm a";
