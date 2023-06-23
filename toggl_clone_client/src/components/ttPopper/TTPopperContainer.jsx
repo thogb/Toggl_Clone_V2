568dfeccb75ec1517a5bc912604d029d2d679c9f
Tao Hu <taohu2222@gmail.com>
2023-06-23 16:28:49 +0800

    Container wrapping around popper content has default padding

---
+++ toggl_clone_client/src/components/ttPopper/TTPopperContainer.jsx
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+import { Box } from "@mui/material";
+import React from "react";
+
+const StyledBox = styled(Box)(({ theme }) => ({
+  padding: "var(--TTPopper-container-padding)",
+}));
+
+const TTPopperContainer = ({ padding, children }) => {
+  const style = {
+    "--TTPopper-container-padding": padding ?? "16px",
+  };
+
+  return <StyledBox style={style}>{children}</StyledBox>;
+};
+
+export default TTPopperContainer;
