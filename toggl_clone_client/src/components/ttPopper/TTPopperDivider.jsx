a6c210fd07ab022540f0e9f13e46b5adee6bdb3e
Tao Hu <taohu2222@gmail.com>
2023-06-23 16:28:11 +0800

    Added divider used in popper

---
+++ toggl_clone_client/src/components/ttPopper/TTPopperDivider.jsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+import { Divider } from "@mui/material";
+import React from "react";
+
+const StyledDivider = styled(Divider)(({ theme }) => ({
+  margin: "0px calc(var(--TTPopper-container-padding) * -1)",
+}));
+
+const TTPopperDivider = ({spacing}) => {
+  return <StyledDivider style={{marginTop: spacing, marginBottom: spacing}}/>;
+};
+
+export default TTPopperDivider;
