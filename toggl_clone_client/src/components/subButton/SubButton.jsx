c6de82e5c855d926f5dfe01f583223f63112e920
Tao Hu <taohu2222@gmail.com>
2023-06-23 16:30:27 +0800

    subButton are small sized button used around different pages

---
+++ toggl_clone_client/src/components/subButton/SubButton.jsx
@@ -0,0 +1,47 @@
+import styled from "@emotion/styled";
+import { ButtonBase } from "@mui/material";
+import { grey } from "@mui/material/colors";
+import React from "react";
+import { Link } from "react-router-dom";
+
+const StyledButton = styled(ButtonBase)(({ theme }) => ({
+  fontSize: theme.typography.caption.fontSize,
+  padding: theme.spacing(1, 1),
+  fontWeight: 600,
+  borderRadius: 8,
+  "&:hover,&:focus,&.TTPopper-open": {
+    backgroundColor: grey[300],
+  },
+  "&:active": {
+    backgroundColor: grey[400],
+  },
+}));
+
+const SubButton = ({
+  to,
+  children,
+  noVerticalPadding,
+  style,
+  onClick,
+  ...others
+}) => {
+  return (
+    <StyledButton
+      component={to ? Link : null}
+      to={to}
+      variant="text"
+      disableRipple
+      disableTouchRipple
+      onClick={onClick}
+      style={{
+        ...(noVerticalPadding && { paddingTop: 0, paddingBottom: 0 }),
+        ...style,
+      }}
+      {...others}
+    >
+      {children}
+    </StyledButton>
+  );
+};
+
+export default SubButton;
