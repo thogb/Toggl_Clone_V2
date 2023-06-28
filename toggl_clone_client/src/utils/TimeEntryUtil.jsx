a3658aba0eba876b93e5830d37309c475ebff47e
Tao Hu <taohu2222@gmail.com>
2023-06-28 15:23:47 +0800

    To store functions used to compare TimeEntry objects

---
+++ toggl_clone_client/src/utils/TimeEntryUtil.jsx
@@ -0,0 +1,16 @@
+// Assumes tags are all sorted in order
+export const isTagDescriptionEqual = (a, b) => {
+  return a.description === b.description && isListEqual(a.tags, b.tags);
+};
+
+// From: https://stackoverflow.com/questions/3115982/how-to-check-if-two-arrays-are-equal-with-javascript
+export const isListEqual = (a, b) => {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+};
