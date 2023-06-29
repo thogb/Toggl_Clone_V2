95bda7b947cd0393e80a7580ef0b0690f76d2777
Tao Hu <taohu2222@gmail.com>
2023-06-29 19:17:24 +0800

    Added group[entry list

--- toggl_clone_client/src/state/store.js
+++ toggl_clone_client/src/state/store.js
@@ -1,11 +1,13 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import currentEntryReducer from "./currentEntrySlice";
 import entryListReducer from "./entryListSlice";
+import groupedEntryListReducer from "./groupedEntryListSlice";
 
 export const store = configureStore({
   reducer: combineReducers({
     currentEntry: currentEntryReducer,
     entryList: entryListReducer,
+    groupedEntryList: groupedEntryListReducer,
   }),
   //   reducer: {
   //     currentEntry: currentEntryReducer,
