506494f07650ce69f69b8c796b30f795d7838f53
Tao Hu <taohu2222@gmail.com>
2023-06-29 19:03:08 +0800

    Stores added to index

--- toggl_clone_client/src/App.js
+++ toggl_clone_client/src/App.js
@@ -1,17 +1,32 @@
 import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 import TimerPage from "./scenes/timerPage/TimerPage";
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 import { themeSettings } from "./theme";
 import ThemePage from "./scenes/themePage/ThemePage";
 import "./App.css";
 import HomePage from "./scenes/homePage/HomePage";
 import NavBar from "./scenes/navbar/NavBar";
+import TestingPage from "./scenes/testingPage/TestingPage";
+import {
+  generateDateGroupedEntries,
+  setDateGroupedEntries,
+} from "./state/groupedEntryListSlice";
+import { getRawEntryList } from "./state/entryListSlice";
+import { useDispatch } from "react-redux";
 
 function App() {
   const mode = "light";
   // const mode = "dark";
+  const dispatch = useDispatch();
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+  useEffect(() => {
+    const dateGroupedEntries = generateDateGroupedEntries(getRawEntryList());
+    // console.log("in app useeffect");
+    // console.log(dateGroupedEntries);
+    dispatch(setDateGroupedEntries({ dateGroupedEntries: dateGroupedEntries }));
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
@@ -23,6 +38,7 @@ function App() {
               <Route path="/" element={<HomePage />} />
               <Route path="/timer" element={<TimerPage />} />
               <Route path="/theme" element={<ThemePage />} />
+              <Route path="/testing" element={<TestingPage />} />
             </Route>
           </Routes>
         </div>
