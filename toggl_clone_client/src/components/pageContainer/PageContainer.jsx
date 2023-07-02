103bc56c92523e54e57a4c443a7c85bd5d86e4ab
Tao Hu <taohu2222@gmail.com>
2023-07-02 21:10:53 +0800

    Some page height chnage

--- toggl_clone_client/src/components/pageContainer/PageContainer.jsx
+++ toggl_clone_client/src/components/pageContainer/PageContainer.jsx
@@ -5,7 +5,7 @@ const bgColor = "#FEF9F7";
 
 const PageContainer = ({ children, ...others }) => {
   return (
-    <Box bgcolor={bgColor} {...others}>
+    <Box bgcolor={bgColor} height={"100vh"} overflow={"scroll"} {...others}>
       {children}
     </Box>
   );
