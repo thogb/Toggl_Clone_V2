457cb2a27c93ac084e2c08105b8e4bf24b332540
Tao Hu <taohu2222@gmail.com>
2023-07-29 12:43:40 +0800

    unused import

--- toggl_clone_client/src/components/timeEntrySection/TimeEntryItem.jsx
+++ toggl_clone_client/src/components/timeEntrySection/TimeEntryItem.jsx
@@ -1,4 +1,4 @@
-import React, { memo, useMemo } from "react";
+import React, { memo } from "react";
 import TimeEntryItemRecord from "./TimeEntryItemRecord";
 import { timeEntryCheckedActions } from "./TimeEntryCheckedReducer";
 import { useDispatch, useSelector } from "react-redux";
