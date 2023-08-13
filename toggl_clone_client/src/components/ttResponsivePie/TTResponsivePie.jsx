811637fc2d4511fc5ef54638d5197ceff4a32a82
Tao Hu <taohu2222@gmail.com>
2023-08-13 19:44:04 +0800

    Added nivo charts. Finished nivo pie chart UI for reports summary page.

---
+++ toggl_clone_client/src/components/ttResponsivePie/TTResponsivePie.jsx
@@ -0,0 +1,99 @@
+import { styled } from "@mui/material";
+import { ResponsivePie } from "@nivo/pie";
+import React, { memo, useMemo } from "react";
+
+const CenteredMetric = ({ label, centerX, centerY }) => {
+  return (
+    <text
+      x={centerX}
+      y={centerY}
+      textAnchor="middle"
+      dominantBaseline="central"
+    >
+      {label}
+    </text>
+  );
+};
+
+const PieWrapper = styled("div")(({ theme }) => ({
+  width: "100%",
+  height: "100%",
+  "& svg": {
+    overflow: "visible",
+  },
+  "&:has(path:hover)": {
+    "& path:not(:hover)": {
+      opacity: 0.4,
+    },
+  },
+  "& path": {
+    cursor: "pointer",
+    "&:hover": {
+      filter: "brightness(0.925)",
+    },
+  },
+}));
+
+const TTResponsivePie = ({ centerText, data, ...others }) => {
+  const total = useMemo(() => {
+    return data.reduce((acc, curr) => acc + curr.value, 0);
+  }, [data]);
+
+  return (
+    <PieWrapper>
+      <ResponsivePie
+        data={data}
+        margin={{ top: 10, right: 20, bottom: 20, left: 20 }}
+        sortByValue={true}
+        innerRadius={0.6}
+        borderColor={{
+          from: "color",
+          modifiers: [["darker", "0"]],
+        }}
+        arcLinkLabel={(e) => `${Math.round((e.value / total) * 100)}%`}
+        arcLinkLabelsTextOffset={0}
+        arcLinkLabelsTextColor="#333333"
+        arcLinkLabelsOffset={15}
+        arcLinkLabelsDiagonalLength={0}
+        arcLinkLabelsStraightLength={0}
+        arcLinkLabelsThickness={0}
+        arcLinkLabelsColor={{ from: "color", modifiers: [] }}
+        enableArcLabels={false}
+        arcLabel={(e) => 3}
+        arcLabelsRadiusOffset={0.8}
+        arcLabelsSkipAngle={10}
+        arcLabelsTextColor={{
+          from: "color",
+          modifiers: [["darker", "1.8"]],
+        }}
+        // tooltip={(e) => {
+        //   let { datum: t } = e;
+        //   // return n.createElement(
+        //   //   l,
+        //   //   { style: { color: t.color } },
+        //   //   n.createElement(s, null, "id"),
+        //   //   n.createElement(d, null, t.id),
+        //   //   n.createElement(s, null, "value"),
+        //   //   n.createElement(d, null, t.value),
+        //   //   n.createElement(s, null, "formattedValue"),
+        //   //   n.createElement(d, null, t.formattedValue),
+        //   //   n.createElement(s, null, "color"),
+        //   //   n.createElement(d, null, t.color)
+        //   // );
+        // }}
+        // animate={false}
+        legends={[]}
+        layers={[
+          "arcs",
+          "arcLabels",
+          "arcLinkLabels",
+          "legends",
+          (props) => <CenteredMetric label={centerText} {...props} />,
+        ]}
+        {...others}
+      />
+    </PieWrapper>
+  );
+};
+
+export default memo(TTResponsivePie);
--- toggl_clone_client/src/scenes/reportsPage/ReportsSummaryPage.jsx
+++ toggl_clone_client/src/scenes/reportsPage/ReportsSummaryPage.jsx
@@ -1,105 +1,79 @@
-import React from "react";
+import React, { useMemo } from "react";
 import ReportsAppbar from "./ReportsAppbar";
 import { Box, Stack, Typography, useTheme } from "@mui/material";
 import TimeBarGraph from "../../components/timeBarGraph/TimeBarGraph";
-import { ResponsivePie } from '@nivo/pie'
+import TTResponsivePie from "../../components/ttResponsivePie/TTResponsivePie";
 
-const MyResponsivePie = ({ data /* see data tab */ }) => (
-  <ResponsivePie
-      data={data}
-      margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-      sortByValue={true}
-      innerRadius={0.6}
-      borderColor={{
-          from: 'color',
-          modifiers: [
-              [
-                  'darker',
-                  '0'
-              ]
-          ]
-      }}
-      arcLinkLabelsTextOffset={0}
-      arcLinkLabelsTextColor="#333333"
-      arcLinkLabelsOffset={15}
-      arcLinkLabelsDiagonalLength={0}
-      arcLinkLabelsStraightLength={0}
-      arcLinkLabelsThickness={0}
-      arcLinkLabelsColor={{ from: 'color', modifiers: [] }}
-      enableArcLabels={false}
-      arcLabel="value"
-      arcLabelsRadiusOffset={0.8}
-      arcLabelsSkipAngle={10}
-      arcLabelsTextColor={{
-          from: 'color',
-          modifiers: [
-              [
-                  'darker',
-                  '1.8'
-              ]
-          ]
-      }}
-      tooltip={e=>{let{datum:t}=e;return n.createElement(l,{style:{color:t.color}},n.createElement(s,null,"id"),n.createElement(d,null,t.id),n.createElement(s,null,"value"),n.createElement(d,null,t.value),n.createElement(s,null,"formattedValue"),n.createElement(d,null,t.formattedValue),n.createElement(s,null,"color"),n.createElement(d,null,t.color))}}
-      animate={false}
-      legends={[]}
-  />)
+const rawPieData = {
+  groups: [
+    {
+      id: 9280720,
+      subGroups: [
+        {
+          id: null,
+          title: "reports page UI",
+          seconds: 17501,
+          ids: [3071257430, 3071076704, 3071142524, 3072878415, 3069398510],
+        },
+        {
+          id: null,
+          seconds: 0,
+          ids: [3075686759],
+        },
+        {
+          id: null,
+          title: "dual n back",
+          seconds: 927,
+          ids: [3071222106],
+        },
+        {
+          id: null,
+          title: "leetcode",
+          seconds: 4740,
+          ids: [3067600245],
+        },
+        {
+          id: null,
+          title: "schulte table",
+          seconds: 2828,
+          ids: [3071205044, 3067564275],
+        },
+      ],
+    },
+  ],
+};
 
-const pieData = {
-  "groups": [
-      {
-          "id": 9280720,
-          "subGroups": [
-              {
-                  "id": null,
-                  "title": "reports page UI",
-                  "seconds": 17501,
-                  "ids": [
-                      3071257430,
-                      3071076704,
-                      3071142524,
-                      3072878415,
-                      3069398510
-                  ]
-              },
-              {
-                  "id": null,
-                  "seconds": 0,
-                  "ids": [
-                      3075686759
-                  ]
-              },
-              {
-                  "id": null,
-                  "title": "dual n back",
-                  "seconds": 927,
-                  "ids": [
-                      3071222106
-                  ]
-              },
-              {
-                  "id": null,
-                  "title": "leetcode",
-                  "seconds": 4740,
-                  "ids": [
-                      3067600245
-                  ]
-              },
-              {
-                  "id": null,
-                  "title": "schulte table",
-                  "seconds": 2828,
-                  "ids": [
-                      3071205044,
-                      3067564275
-                  ]
-              }
-          ]
-      }
-  ]
-}
+const pieData = rawPieData.groups[0].subGroups.map((item, ind) => {
+  return {
+    id: ind,
+    label: item.title ?? "",
+    value: item.seconds,
+  };
+});
 
 const ReportsSummaryPage = () => {
   const theme = useTheme();
+
+  const usersPieData = useMemo(() => {
+    return rawPieData.groups.map((item, ind) => {
+      return {
+        id: item.id,
+        value: item.subGroups.reduce((acc, curr) => {
+          return acc + curr.seconds;
+        }, 0),
+      };
+    });
+  }, []);
+
+  const timeEntriesPieData = useMemo(() => {
+    return rawPieData.groups[0].subGroups.map((item, ind) => {
+      return {
+        id: ind,
+        label: item.title ?? "",
+        value: item.seconds,
+      };
+    });
+  }, []);
 
   return (
     <>
@@ -170,15 +144,41 @@ const ReportsSummaryPage = () => {
           resolution: "day",
         }}
       />
-      <Stack direction={"row"} bgcolor={"green"} mx={
-        theme.spacing(theme.ttSpacings.page.px)
-      }>
+      <Stack
+        direction={"row"}
+        // bgcolor={"green"}
+        mx={theme.spacing(theme.ttSpacings.page.px)}
+      >
         <Box bgcolor={"grey"} flexGrow={1} flexShrink={0}>
-asd
+          asd
         </Box>
-        <Stack flexShrink={0} direction={"column"} alignItems={"center"}>
-          <Typography variant="subtitle2" textTransform={"uppercase"}>Total Hours</Typography>
-          <Typography variant="h6">7:31:30</Typography>
+        <Stack
+          flexShrink={0}
+          direction={"column"}
+          alignItems={"center"}
+          pl={"20px"}
+          pr={{ xs: 0, md: "120px" }}
+        >
+          <Typography
+            textAlign={"start"}
+            variant="subtitle2"
+            textTransform={"uppercase"}
+            width={"100%"}
+            px={1}
+          >
+            Total Hours
+          </Typography>
+          <Typography textAlign={"start"} variant="h5" width={"100%"} px={1}>
+            7:31:30
+          </Typography>
+          <Box my={3} width={"220px"} height={"220px"}>
+            <TTResponsivePie data={usersPieData} centerText={"Users"} />
+            <Box py={1} />
+            <TTResponsivePie
+              data={timeEntriesPieData}
+              centerText={"Time Entries"}
+            />
+          </Box>
         </Stack>
       </Stack>
     </>
